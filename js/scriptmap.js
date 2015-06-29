d10373ba47ac685bed640dc0689358f6b6d79589
deriojunior <derio.junior@hotmail.com>
2015-06-29 12:50:30 -0300

    map in process

--- js/scriptmap.js
+++ js/scriptmap.js
@@ -1,9 +1,22 @@
-      function initialize() {
+
+
+function initialize() {
         var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: { lat: -22.2202467, lng: -45.9155238},
+          zoom: 17
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+          
+        var marker = new google.maps.Marker({
+            position : new google.maps.LatLng(ponto.lat, ponto.lng),
+            title: "Your Pizza!",
+            map: map,
+            icon:'img/markerpizza.png'
+});  
+                   
+          
       }
       google.maps.event.addDomListener(window, 'load', initialize);
+
+    
